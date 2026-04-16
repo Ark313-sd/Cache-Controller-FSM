f5b9bb60fc4e17ea62743c2b186f50fc125488d9
Ark313-sd <saad.tahsin@gmail.com>
2026-04-16 21:05:03 +0600

    Update Controller.hpp

--- Controller.hpp
+++ Controller.hpp
@@ -15,13 +15,14 @@ class Controller
     State state; 
     std::queue<Instruction> pendingInstructions;
     bool fetchBlock(const std::string addr);
-    std::string getTag(const std::string& addr) const;
-    bool hit(const std::string& addr) const;
-    int getBlockIdx(const std::string& addr) const;
-    int getByteOffset(const std::string& addr) const;
-    int getWordOffset(const std::string& bits) const;
-    int getDataFromCacheByAddr(Instruction::instructionType iType, const std::string& addr) const;
-    bool writeBackLine(int lineIdx);
+std::string getTag(const std::string& addr) const;
+bool hit(const std::string& addr) const;
+int getBlockIdx(const std::string& addr) const;
+int getByteOffset(const std::string& addr) const;
+int getWordOffset(const std::string& bits) const;
+int getDataFromCacheByAddr(Instruction::instructionType iType, const std::string& addr) const;
+bool writeBackLine(int lineIdx);
+bool handleMiss(const std::string& addr);
 
 public:
     Controller(Cache* cache);
